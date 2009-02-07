e020b5f492bfa19ce26d53950dfa4a6fdd254468
Jouni Korhonen <jokor@bittivasara.(none)>
2009-02-07 14:59:36 +0200

    Liitos kuntoon customers ja users taulujen välille

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,3 @@
 class Customer < ActiveRecord::Base
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 require 'digest/sha1'
 
 class User < ActiveRecord::Base
+  belongs_to :customer
+
   include Authentication
   include Authentication::ByPassword
   include Authentication::ByCookieToken
@@ -18,7 +20,7 @@ class User < ActiveRecord::Base
   validates_uniqueness_of   :email
   validates_format_of       :email,    :with => Authentication.email_regex, :message => Authentication.bad_email_message
 
-  
+
 
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
@@ -29,7 +31,7 @@ class User < ActiveRecord::Base
 
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
   #
-  # uff.  this is really an authorization, not authentication routine.  
+  # uff.  this is really an authorization, not authentication routine.
   # We really need a Dispatch Chain here or something.
   # This will also let us return a human error message.
   #
@@ -48,7 +50,7 @@ class User < ActiveRecord::Base
   end
 
   protected
-    
+
 
 
 end
