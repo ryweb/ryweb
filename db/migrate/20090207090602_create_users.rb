2fa503dcebc6ec5d7c1ce2e64679014798748636
Jouni Korhonen <jokor@bittivasara.snet.fi>
2009-02-07 13:09:57 +0200

    customer_id kenttä user tauluun

--- db/migrate/20090207090602_create_users.rb
+++ db/migrate/20090207090602_create_users.rb
@@ -10,7 +10,7 @@ class CreateUsers < ActiveRecord::Migration
       t.column :updated_at,                :datetime
       t.column :remember_token,            :string, :limit => 40
       t.column :remember_token_expires_at, :datetime
-
+      t.column :customer_id,               :integer
 
     end
     add_index :users, :login, :unique => true
