84a3dc3879c569c2b5dee12ac184604fc9d4db1a
Jussi Korhonen <juskor@gmail.com>
2009-02-16 05:34:19 +0800

    Käyttäjien listaaminen ja käyttäjän lisääminen
    Signed-off-by: jokor <jpkorhonen@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,15 @@ class UsersController < ApplicationController
     @user = self.current_user
   end
   
+    def list
+    @users = User.find(:all)
+
+    respond_to do |format|
+      format.html # list.html.erb
+      format.xml  { render :xml => @users }
+    end
+  end
+  
   # render new.rhtml
   def new
     @user = User.new
@@ -27,17 +36,18 @@ class UsersController < ApplicationController
   end
   
   def create
-    logout_keeping_session!
     @user = User.new(params[:user])
     success = @user && @user.save
     if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
+      @user.update_attribute(:customer_id,current_user.customer_id)
+
+      # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
-      self.current_user = @user # !! now logged in
-      redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+
+      redirect_to(user_url (:id => @user))
+      flash[:notice] = "Uusi käyttäjä lisätty onnistuneesti."
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
       render :action => 'new'
