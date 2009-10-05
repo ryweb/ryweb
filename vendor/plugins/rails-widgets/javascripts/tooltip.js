1a9f8722ecec414f957ff8b02f81192e76c1da1e
Jussi Korhonen <juskor@gmail.com>
2009-10-05 18:16:07 +0300

    Pikkufiksejä tooltippiin

--- vendor/plugins/rails-widgets/javascripts/tooltip.js
+++ vendor/plugins/rails-widgets/javascripts/tooltip.js
@@ -1,4 +1,37 @@
 // tooltip widget
+/*function toggleTooltip(event, element) { 
+  var __x = Event.pointerX(event);
+  var __y = Event.pointerY(event);
+  //alert(__x+","+__y);
+  element.style.top = __y + 5;  
+  element.style.left = __x - 40;
+  element.toggle();
+}*/
+
+// tooltip widget
+function positionTooltip(event, element){
+var __x = Event.pointerX(event);
+  var __y = Event.pointerY(event);
+  //alert(__x+","+__y);
+  element.style.top = __y + 5;  
+  element.style.left = __x - 40;
+}
+
+function toggleTooltip(event, element) { 
+  positionTooltip(event, element);
+  element.toggle();
+}
+
+function showTooltip(event, element) { 
+  positionTooltip(event, element);
+  element.show();
+}
+
+function hideTooltip(event, element) { 
+  positionTooltip(event, element);
+  element.hide();
+}
+// tooltip widget
 function toggleTooltip(event, element) { 
   var __x = Event.pointerX(event);
   var __y = Event.pointerY(event);
