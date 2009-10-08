0b30e71c34899a60e700a1428b5c022824411016
Jussi Korhonen <juskor@gmail.com>
2009-10-08 21:30:55 +0300

    Tooltipin positiointi oli IE:llä täysin metsässä. Tehty pientä korjausta asiaan.

--- public/javascripts/widgets/tooltip.js
+++ public/javascripts/widgets/tooltip.js
@@ -1,20 +1,11 @@
 // tooltip widget
-/*function toggleTooltip(event, element) { 
-  var __x = Event.pointerX(event);
-  var __y = Event.pointerY(event);
-  //alert(__x+","+__y);
-  element.style.top = __y + 5;  
-  element.style.left = __x - 40;
-  element.toggle();
-}*/
-
-// tooltip widget
 function positionTooltip(event, element){
 var __x = Event.pointerX(event);
   var __y = Event.pointerY(event);
   //alert(__x+","+__y);
-  element.style.top = __y + 5;  
-  element.style.left = __x - 40;
+  element.style.position = 'absolute'
+  element.style.top = __y - 170 + 'px';  
+  element.style.left = __x - 265 + 'px';
 }
 
 function toggleTooltip(event, element) { 
