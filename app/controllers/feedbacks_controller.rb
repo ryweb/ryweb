49431da3eb664114117e7be8e55f3ff244e9824e
Jussi Korhonen <juskor@gmail.com>
2009-10-18 20:35:20 +0800

    simppelin palautelomake pilottikäyttäjien ideoiden ja palautteiden kokoamiseksi

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,55 @@
+class FeedbacksController < ApplicationController
+  before_filter :login_required
+
+
+  # GET /feedbacks/1
+  # GET /feedbacks/1.xml
+  def show
+    @feedback = Feedback.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @feedback }
+    end
+  end
+
+  # GET /feedbacks/new
+  # GET /feedbacks/new.xml
+  def new
+    @feedback = Feedback.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @feedback }
+    end
+  end
+
+  # POST /feedbacks
+  # POST /feedbacks.xml
+  def create
+    @feedback = Feedback.new(params[:feedback])
+
+    respond_to do |format|
+      if @feedback.save
+        flash[:notice] = 'Palaute lähetetty.'
+        format.html { redirect_to users_url }
+        format.xml  { render :xml => @feedback, :status => :created, :location => @feedback }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @feedback.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /feedbacks/1
+  # DELETE /feedbacks/1.xml
+  def destroy
+    @feedback = Feedback.find(params[:id])
+    @feedback.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(feedbacks_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
