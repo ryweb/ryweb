9cccc5efa6e3433ffe43d257cc4d61a35827bcd9
juskor <juskor@gmail.com>
2010-04-17 18:31:11 +0300

    IE tarvitsee uuden tiedostotyypin jpg kuville

--- app/models/graphic.rb
+++ app/models/graphic.rb
@@ -2,7 +2,7 @@ class Graphic < CustomerData
 has_attachment  :storage => :file_system,
                   :max_size => 1.megabytes,
                   :min_size => 0.kilobytes,
-                  :content_type => ['image/jpeg', 'image/gif', 'image/png'],
+                  :content_type => ['image/jpeg', 'image/gif', 'image/png', 'image/pjpeg'],
                   :path_prefix => 'public/images'
 
   validates_as_attachment
