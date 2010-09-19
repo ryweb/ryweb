c0ddb8a3f0fee8b88dc60b73547668f64dfd14f4
Olli Saari <ollisaari@a88-112-140-233.elisa-laajakaista.fi>
2010-09-19 11:16:27 +0300

    Hallintapuolen käyttliittymän viilausta

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(document).ready(function($){
+	
+	/* Buttons */
+	$('a.button').each(function() {
+		var Text = $(this).text();
+		var Class = $(this).attr('class');
+		$(this).html('<span><span>'+ Text +'</span></span>');
+		if(Class.search("icon_") > 0) {
+			$(this).attr('class', 'button');
+			$(this).children('span').children('span').attr('class', Class +' icon');
+			$(this).children('span').children('span').removeClass('button');
+		}
+	});
+	
+
+});
